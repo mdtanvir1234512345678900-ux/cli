bfb2feec56c8ee6efc8d04a3d35710f0efd6e056
Security Research <mdtanvir1234512345678900@gmail.com>
2026-07-01 22:41:32 +0600

    Add newline at end of base.ts
    Fix formatting by adding a newline at the end of the file.

--- src/lib/base.ts
+++ src/lib/base.ts
@@ -129,5 +129,5 @@ export abstract class BaseCommand<T extends typeof Command> extends Command {
     const data = input.map((i) => toRow(i))
     console.table(data)
   }
-        }
-  
+  }
+      
